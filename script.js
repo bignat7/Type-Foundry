e2d0c7e3636c9bdcba1720fddb34e1d5d4607e60
Natalie Ho <natalie95030@icloud.com>
2024-12-08 15:27:18 -0500

    added landing

--- script.js
+++ script.js
@@ -39,3 +39,61 @@
     // Hide the typeface details section
     typefaceDetails.style.display = "none";
   }
+
+  var granimInstance = new Granim({
+    element: '#canvas-interactive',
+    name: 'interactive-gradient',
+    elToSetClassOn: '.canvas-interactive-wrapper',
+    direction: 'diagonal',
+    isPausedWhenNotInView: true,
+    stateTransitionSpeed: 500,
+    states : {
+        "default-state": {
+            gradients: [
+                ['#1656a4', '#04101E'],
+                ['#744A15', '#1656a4'],
+                ['#1656a4', '#877E3B'],
+                ['#C6ABBD', '#1656a4'],
+                ['#1656a4', '#1656a4'],
+            ],
+            transitionSpeed: 8000
+        },
+        "violet-state": {
+            gradients: [
+                ['#9D50BB', '#6E48AA'],
+                ['#4776E6', '#8E54E9']
+            ],
+            transitionSpeed: 2000
+        },
+        "orange-state": {
+            gradients: [ ['#FF4E50', '#F9D423'] ],
+            loop: false
+        }
+    }
+});
+
+// With jQuery
+$('#default-state-cta').on('click', function(event) {
+    event.preventDefault();
+    granimInstance.changeState('default-state');
+    setClass('#default-state-cta')
+});
+$('#violet-state-cta').on('click', function(event) {
+    event.preventDefault();
+    granimInstance.changeState('violet-state');
+    setClass('#violet-state-cta')
+});
+$('#orange-state-cta').on('click', function(event) {
+    event.preventDefault();
+    granimInstance.changeState('orange-state');
+    setClass('#orange-state-cta')
+});
+
+function setClass(element) {
+    $('.canvas-interactive-wrapper a').removeClass('active');
+    $(element).addClass('active');
+};
+
+window.addEventListener('load', function () {
+  document.querySelector('.fade-in-bg').classList.add('visible');
+});
